f7bf802cbfee8e336c79771ef0547079c2bb5381
Tyler Ohlsen <tylerohlsen@hotmail.com>
2015-03-09 22:21:10 -0500

    Added 20210463

---
+++ StackOverflow_20210463/StackOverflow_20210463/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AppServices
+{
+    public class App : IApp
+    {
+        public cSecurity _csec;
+
+        public string GetItems(int agentID, string agentName)
+        {
+            // Need to use some functions from the cSecurity class here???
+            return _csec.getItems();
+        }
+    }
+}
---
+++ StackOverflow_20210463/StackOverflow_20210463/cSecurity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Configuration;
+using System.Net;
+
+namespace AppServices
+{
+    public class cSecurity
+    {
+        // Some functions defined here....
+
+        public string getItems()
+        {
+            return string.Empty;
+        }
+    }
+}
