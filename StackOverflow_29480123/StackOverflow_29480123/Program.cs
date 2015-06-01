e3b9fa18eae64312b98bfc9ed5f1ef727f13fa61
Tyler Ohlsen <tylerohlsen@hotmail.com>
2015-05-31 22:17:16 -0500

    Added 29460123

---
+++ StackOverflow_29480123/StackOverflow_29480123/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
+
+namespace StackOverflow_29480123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new UnityContainer();
+            container.AddExtension(new Interception());
+
+            container.RegisterTypes(
+                AllClasses.FromLoadedAssemblies().WithMatchingInterface(),
+                WithMappings.FromMatchingInterface,
+                WithName.Default,
+                WithLifetime.ContainerControlled,
+                getInjectionMembers: t => new InjectionMember[]
+                {
+                    new Interceptor<InterfaceInterceptor>(),
+                    new InterceptionBehavior<PolicyInjectionBehavior>(),
+                });
+
+            var controller = container.Resolve<MyController>();
+        }
+    }
+
+    public static class TypeFilters
+    {
+        public static IEnumerable<Type> WithMatchingInterface(this IEnumerable<Type> types)
+        {
+            return types.Where(type => type.GetTypeInfo().GetInterface("I" + type.Name) != null);
+        }
+    }
+
+    public interface IFoo
+    {
+        int GetData();
+    }
+
+    public class Foo : IFoo
+    {
+        public int GetData()
+        {
+            return 1;
+        }
+    }
+
+    public class MyController
+    {
+        [Dependency]
+        public IFoo Foo { get; set; }
+    }
+}
