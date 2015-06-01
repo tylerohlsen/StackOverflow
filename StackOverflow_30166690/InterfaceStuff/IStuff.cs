12967158874307269ead83037161556841ee2f68
Tyler Ohlsen <tylerohlsen@hotmail.com>
2015-05-31 22:19:09 -0500

    Added 30166690

---
+++ StackOverflow_30166690/InterfaceStuff/IStuff.cs
@@ -0,0 +1,7 @@
+﻿namespace InterfaceStuff
+{
+    public interface IEmployeeDataService
+    {
+         
+    }
+}
---
+++ StackOverflow_30166690/StackOverflow_30166690/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceStuff;
+using Microsoft.Practices.Unity;
+
+namespace StackOverflow_30166690
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new UnityContainer();
+
+            container.RegisterTypes(AllClasses.FromLoadedAssemblies(),
+                WithMappings.FromMatchingInterface,
+                WithName.Default,
+                WithLifetime.Transient);
+
+            var controller =  container.Resolve<EmployeeController>();
+        }
+    }
+
+    public class EmployeeController
+    {
+        private IEmployeeDataService dataService;
+
+        public EmployeeController(IEmployeeDataService dataService)
+        {
+            this.dataService = dataService;
+        }
+    }
+}
